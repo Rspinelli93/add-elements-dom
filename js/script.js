cbc2220c1687c5b583a962a820f36b2a5c5eda3c
RiccardoS <rspinellir13@gmail.com>
2024-11-06 16:33:41 +0100

    Respuesta 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
  // Aquí tu código
+
+ const boton = document.getElementById("agregar");
+ const lista = document.getElementById("lista");
+
+console.log(boton)
+console.log(lista)
+
+boton.addEventListener("click", function() {
+    let cartel = prompt("Imprime en pantalla:");
+    
+    if (cartel) {
+        let nuevoLi = document.createElement("li");
+
+        const respuesta = document.createTextNode(cartel);
+
+        nuevoLi.appendChild(respuesta);
+
+        lista.appendChild(nuevoLi);
+    }
+});
+
+
+
+
